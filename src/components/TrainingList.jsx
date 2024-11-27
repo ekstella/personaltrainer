8a18c12a994b713c3a27e0ac2881cd98a939ee19
Ella Ekström <eltsu.ekstrom@gmail.com>
2024-11-27 19:35:47 +0200

    fix: date format

--- src/components/TrainingList.jsx
+++ src/components/TrainingList.jsx
@@ -1,11 +1,12 @@
 import DataList from "./DataList";
 import { useEffect, useState } from "react";
+import { format, parseISO } from "date-fns";
 
 const TrainingList = () => {
   const [rowData, setRowData] = useState([]);
 
   const [colDefs, setColDefs] = useState([
-    { field: "date" }, //TODO: format date
+    { field: "date", valueFormatter: (date) => formatDate(date.data.date) }, //TODO: format date
     { field: "duration" },
     { field: "activity" },
     {
@@ -14,6 +15,12 @@ const TrainingList = () => {
         row.data.customer.firstname + " " + row.data.customer.lastname,
     },
   ]);
+
+  const formatDate = (dateString) => {
+    console.log(dateString)
+    const date = parseISO(dateString);
+    return format(date, "dd.MM.yyyy hh:mm");
+  };
 
   const getData = () => {
     fetch(
