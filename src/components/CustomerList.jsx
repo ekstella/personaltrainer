0c4fc12b85e8377851c14092029872f3468de5dc
Ella Ekström <eltsu.ekstrom@gmail.com>
2024-11-27 18:31:21 +0200

    feat: fetch customer data

--- src/components/CustomerList.jsx
+++ src/components/CustomerList.jsx
@@ -1,7 +1,31 @@
-import DataList from "./DataList"
+import DataList from "./DataList";
+import { useEffect, useState } from "react";
 
 const CustomerList = () => {
-    return <DataList/>
-}
+  const [rowData, setRowData] = useState([]);
+
+  const [colDefs, setColDefs] = useState([
+    { field: "firstname" },
+    { field: "lastname" },
+    { field: "streetaddress" },
+    { field: "postcode" },
+    { field: "city" },
+    { field: "email" },
+    { field: "phone" },
+  ]);
+  const getData = () => {
+    fetch(
+      "https://customer-rest-service-frontend-personaltrainer.2.rahtiapp.fi/api/customers"
+    )
+      .then((resp) => resp.json())
+      .then((data) => {
+        setRowData(data._embedded.customers);
+      });
+  };
+
+  useEffect(getData, []);
+
+  return <DataList rowData={rowData} colDefs={colDefs} />;
+};
 
 export default CustomerList;
